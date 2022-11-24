aef9a1e75a21d435b21eb9f893941f2111154ee2
jkulka <kulkajens82@gmail.com>
2022-11-24 12:29:16 +0100

    WOHO

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 13:46:44 by jkulka            #+#    #+#             */
+/*   Updated: 2022/11/24 12:19:18 by jkulka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "libft.h"
+
+int		ft_printf(const char *str, ...);
+#endif
