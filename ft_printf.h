70aa1e448a66aaf98e93e25df2263e4ebc69e618
jkulka <kulkajens82@gmail.com>
2023-03-17 13:48:04 +0100

    removed bastard functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jkulka <jkulka@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 13:46:44 by jkulka            #+#    #+#             */
-/*   Updated: 2023/03/17 13:25:53 by jkulka           ###   ########.fr       */
+/*   Updated: 2023/03/17 13:45:33 by jkulka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,19 @@
 # include <unistd.h>
 
 int		ft_printf(const char *str, ...);
-char	*ft_convert_base10_to_hex(int base10_num);
-char	*ft_convert_base10_to_hex_lower(int base10_num);
-void ft_check_arg(const char c, va_list *arg, int *count);
-int	ft_printchar(char arg);
-int	ft_printstr(char *arg);
-int	ft_printint(int arg);
-int ft_printuint(unsigned int arg);
-int ft_printhexlow(unsigned long arg);
-int	ft_printhexup(unsigned long arg);
-int	ft_printpercent(void);
-int ft_printptr(unsigned long ptr);
-char *ft_ptoa(long n);
-char *ft_prefix(char *prefix, char *str);
+void	ft_check_arg(const char c, va_list *arg, int *count);
+int		ft_printchar(char arg);
+int		ft_printstr(char *arg);
+int		ft_printint(int arg);
+int		ft_printuint(unsigned int arg);
+int		ft_printhexlow(unsigned long arg);
+int		ft_printhexup(unsigned long arg);
+int		ft_printpercent(void);
+int		ft_printptr(unsigned long ptr);
+char	*ft_ptoa(long n);
+char	*ft_prefix(char *prefix, char *str);
 size_t	ft_digit_len(unsigned long n, int base);
-char *ft_itoa_base(unsigned long n, int low);
-char *ft_uitoa(unsigned int n);
+char	*ft_itoa_base(unsigned long n, int low);
+char	*ft_uitoa(unsigned int n);
 
 #endif
